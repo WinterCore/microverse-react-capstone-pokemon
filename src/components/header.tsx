4bb0089382aeba65e31852bd660067b71ee7a3db
WinterCore <hogobbl@gmail.com>
2021-01-12 23:31:12 +0200

    Make the sign in the header link to the home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import React      from 'react';
 import classnames from 'classnames';
+import { Link }   from 'react-router-dom';
 
 import styles     from './header.module.css';
 import utilStyles from '../utility.module.css';
@@ -12,12 +13,13 @@ const Header: React.FC = () => {
             <div className={ classnames(styles.descriptionBar, utilStyles.flex, utilStyles.justifyCenter) }>
                 <div className={ classnames(utilStyles.container, styles.topHeader) }>
                     Welcome to Pokemonipedia, where you can view pokemon types and look at pokemons.
-                    <div
+                    <Link
+                        to="/"
                         className={ classnames(utilStyles.flex, utilStyles.column, utilStyles.justifyCenter, utilStyles.alignCenter, styles.sign) }
                     >
                         <img alt="logo" src={ logo } />
                         <div className={ styles.signText }>Pokipedia</div>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </header>
