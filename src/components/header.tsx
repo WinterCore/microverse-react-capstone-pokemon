bcec4c7496c316c7813a7522f31d6d92feaac401
WinterCore <hogobbl@gmail.com>
2021-01-13 02:54:46 +0200

    Fix typos and update names

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,13 +12,13 @@ const Header: React.FC = () => {
         <header>
             <div className={ classnames(styles.descriptionBar, utilStyles.flex, utilStyles.justifyCenter) }>
                 <div className={ classnames(utilStyles.container, styles.topHeader) }>
-                    Welcome to Pokemonipedia, where you can view pokemon types and look at pokemons.
+                    Welcome to Pokepedia, where you can view pokemon types and look at pokemons.
                     <Link
                         to="/"
                         className={ classnames(utilStyles.flex, utilStyles.column, utilStyles.justifyCenter, utilStyles.alignCenter, styles.sign) }
                     >
                         <img alt="logo" src={ logo } />
-                        <div className={ styles.signText }>Pokipedia</div>
+                        <div className={ styles.signText }>Pokepedia</div>
                     </Link>
                 </div>
             </div>
